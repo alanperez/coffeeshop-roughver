285162a89467c1f7979b307819dd3b6c33344754
unknown <alanperez9six@gmail.com>
2021-01-07 17:34:00 -0600

    dashboard wip, admin crud operations fully functioning

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,14 +5,14 @@
 @section('content')
 
             <div class="dashboard-ecommerce">
-                <div class="container-fluid dashboard-content ">
+                <!-- <div class="container-fluid dashboard-content ">
                     <!-- ============================================================== -->
                     <!-- pageheader  -->
                     <!-- ============================================================== -->
                     <div class="row">
                         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                             <div class="page-header">
-                                <h2 class="pageheader-title">E-commerce Dashboard Template </h2>
+                                <h2 class="pageheader-title">Dashboard Template </h2>
                                 <p class="pageheader-text">Nulla euismod urna eros, sit amet scelerisque torton lectus vel mauris facilisis faucibus at enim quis massa lobortis rutrum.</p>
                                 <div class="page-breadcrumb">
                                     <nav aria-label="breadcrumb">
@@ -524,7 +524,7 @@
                             <!-- ============================================================== -->
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
 
 @endsection
